af7be259c5e4c6b5402abaddeceb3482bfea7fbc
LucasManete <lucas.manete2403@gmail.com>
2022-06-20 19:04:19 -0300

    Fix: model de user

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,5 +1,6 @@
 const UserSchema = (Sequelize, DataTypes) => {
   const UserTable = Sequelize.define("User", {
+    id: DataTypes.INTEGER,
     displayName: DataTypes.String,
     email: DataTypes.String,
     password: DataTypes.String,
